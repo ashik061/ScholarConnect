a032fe01d6c8290b56d7ef53370b34211b581ca5
ashik061 <ashikurrahman.24csedu.061@gmail.com>
2024-07-08 15:29:36 +0600

    Still trying. Couldn't fix the bug yet.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000,
     proxy: {
       '/api': {
         target: 'https://my-json-server.typicode.com/ashik061/json-backend-server/mentors',
